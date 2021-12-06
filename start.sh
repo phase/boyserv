7d5888502c594c61f268a99921d036b14a290d53
jado <j@jadon.io>
2021-12-06 15:52:27 -0700

    Initial Commit

---
+++ start.sh
@@ -0,0 +1,17 @@
+# Script for backing up worlds, pulling latest changes, and starting the server.
+
+while true
+do
+  DATE=$(date +"%d-%b-%Y-%H-%M-%S")
+  mkdir -p backup/
+  tar -zcvf world-$DATE.tgz world
+  mv *.tgz backup/
+
+  git reset --hard HEAD
+  git pull
+
+  java -server \
+  -Xms8G -Xmx8G \
+  -jar fabric-server-launch.jar nogui
+
+done
